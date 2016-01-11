5bb98a2887ea01295a40435d0601c0b267a0c8c3
yudiwbs <yudi1975@gmail.com>
2016-01-12 05:48:44 +0700

    Initial commit

---
+++ src/com/yuliadi/CrawlerSE.java
@@ -0,0 +1,30 @@
+package com.yuliadi;
+
+/**
+ * Created by yudiwbs on 12/01/2016.
+ */
+
+public class CrawlerSE {
+    private void connectToDB() {
+        String usrName="";
+        String pwd="";
+    }
+
+    public void proses() {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        CrawlerSE cg = new CrawlerSE();
+        cg.proses();
+        /*
+          //sleep dengan waktu random
+          //saat jalan crawl:
+            //cek id terakhir yang sudah diproses, inc id
+            //ambil berdasarkankan id, teks H yang akan diproses
+            //panggil API
+            //simpan hasil
+            //tulis status
+        */
+    }
+}
