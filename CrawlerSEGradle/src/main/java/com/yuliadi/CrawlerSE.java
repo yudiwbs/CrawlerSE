1411ab6c6151fa29983a8cb86ddfaa0e5c1f4658
yudiwbs <yudi1975@gmail.com>
2016-01-12 15:30:14 +0700

    test db

--- CrawlerSEGradle/src/main/java/com/yuliadi/CrawlerSE.java
+++ CrawlerSEGradle/src/main/java/com/yuliadi/CrawlerSE.java
@@ -1,13 +1,42 @@
 package com.yuliadi;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  * Created by yudiwbs on 12/01/2016.
  */
 
 public class CrawlerSE {
-    private void connectToDB() {
-        String usrName="";
-        String pwd="";
+
+    private void ambilData() {
+        String usrName="yudilocal";
+        String pwd="yudilocal";
+        String dbName="searchengine";
+        Connection conn=null;
+        PreparedStatement pSel=null;
+        ResultSet rs = null;
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/"+dbName
+                    + "?user="+usrName+"&password="+pwd);
+            pSel = conn.prepareStatement("select id,t,h from rte3_babak2 " +
+                    " where id=?");
+            pSel.setString(1,"1");
+            rs = pSel.executeQuery();
+            while (rs.next()) {
+                String nim = rs.getString(1);
+                String nama = rs.getString(2);
+                System.out.println(nim+","+nama);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void proses() {
@@ -16,7 +45,10 @@ public class CrawlerSE {
 
     public static void main(String[] args) {
         CrawlerSE cg = new CrawlerSE();
-        cg.proses();
+        cg.ambilData();
+
+        //cg.proses();
+
         /*
           //sleep dengan waktu random
           //saat jalan crawl:
